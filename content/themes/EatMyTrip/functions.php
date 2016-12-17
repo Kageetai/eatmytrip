44666862cad14ca43afd5e407be7bf7084ad40aa
Kageetai <ma@kageetai.net>
2016-12-17 02:59:25 +0100

    add flexslider.css to theme style dependencies

--- content/themes/EatMyTrip/functions.php
+++ content/themes/EatMyTrip/functions.php
@@ -15,6 +15,7 @@ add_action( 'wp_enqueue_scripts', 'chld_thm_cfg_parent_css', 10 );
 function child_theme_configurator_css() {
 	wp_enqueue_style( 'chld_thm_cfg_separate', get_stylesheet_directory_uri() . '/css/main.css', array(
 		'chld_thm_cfg_parent',
+		'flexslider',
 		'style',
 		'font-awesome',
 		'animate'
