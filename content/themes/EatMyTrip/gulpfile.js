eceec069c67d769bc29f1e018bb0b4b4467e11bd
Kageetai <ma@kageetai.net>
2017-02-18 22:58:37 +0100

    updated gulpfile

--- content/themes/EatMyTrip/gulpfile.js
+++ content/themes/EatMyTrip/gulpfile.js
@@ -5,65 +5,74 @@ var plumber = require('gulp-plumber');
 var rename = require('gulp-rename');
 var sourcemaps = require('gulp-sourcemaps');
 var sass = require('gulp-sass');
+var csslint = require('gulp-csslint');
 var autoPrefixer = require('gulp-autoprefixer');
 //if node version is lower than v.0.1.2
 require('es6-promise').polyfill();
 var cssComb = require('gulp-csscomb');
 var cmq = require('gulp-merge-media-queries');
 var cleanCss = require('gulp-clean-css');
+var jshint = require('gulp-jshint');
 var uglify = require('gulp-uglify');
+// var notify = require('gulp-notify');
 
 gulp.task('scss', function () {
-    gulp.src(['scss/main.scss'])
-        .pipe(plumber({
-            handleError: function (err) {
-                console.log(err);
-                this.emit('end');
-            }
-        }))
-        .pipe(sourcemaps.init())
-        .pipe(sass())
-        .pipe(autoPrefixer())
-        .pipe(cssComb())
-        .pipe(cmq({log: true}))
-        .pipe(gulp.dest('css'))
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(cleanCss())
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('css'))
-        .pipe(browserSync.stream());
+  gulp.src(['scss/**/*.scss'])
+      .pipe(plumber({
+        handleError: function (err) {
+          console.log(err);
+          this.emit('end');
+        }
+      }))
+      // .pipe(sourcemaps.init())
+      .pipe(sass())
+      .pipe(autoPrefixer())
+      .pipe(cssComb())
+      .pipe(cmq({log: true}))
+      .pipe(csslint())
+      .pipe(csslint.formatter())
+      .pipe(gulp.dest('css'))
+      .pipe(rename({
+        suffix: '.min'
+      }))
+      .pipe(cleanCss())
+      // .pipe(sourcemaps.write())
+      .pipe(gulp.dest('css'))
+      .pipe(reload({stream: true}));
+      // .pipe(notify('css task finished'))
 });
 
 gulp.task('js', function () {
-    gulp.src(['js/**/*.js'])
-        .pipe(plumber({
-            handleError: function (err) {
-                console.log(err);
-                this.emit('end');
-            }
-        }))
-        .pipe(gulp.dest('js'))
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(uglify())
-        .pipe(gulp.dest('js'))
-        .pipe(browserSync.stream());
+  gulp.src(['js/**/*.js'])
+      .pipe(plumber({
+        handleError: function (err) {
+          console.log(err);
+          this.emit('end');
+        }
+      }))
+      .pipe(jshint())
+      .pipe(jshint.reporter('default'))
+      .pipe(gulp.dest('js'))
+      .pipe(rename({
+        suffix: '.min'
+      }))
+      .pipe(uglify())
+      .pipe(gulp.dest('js'))
+      .pipe(reload());
+      // .pipe(notify('js task finished'))
 });
 
 gulp.task('default', ['js', 'scss'], function () {
-    browserSync.init({
-        files: ['**/*.php', '*.php', 'css/*.css', 'js/*.js'],
-        open: false,
-        proxy: "http://localhost/eatmytrip/",
-        snippetOptions: {
-            whitelist: ['/wp-admin/admin-ajax.php'],
-            blacklist: ['/wp-admin/**']
-        }
-    });
-    gulp.watch('js/**/*.js', ['js']);
-    gulp.watch('scss/**/*.scss', ['scss']);
-    gulp.watch('images/src/**/*', ['image']);
+  browserSync.init({
+    files: ['**/*.php', '*.php', 'css/*.css', 'js/*.js'],
+    open: false,
+    proxy: "http://localhost/eatmytrip/",
+    snippetOptions: {
+      whitelist: ['/wp-admin/admin-ajax.php'],
+      blacklist: ['/wp-admin/**']
+    }
+  });
+  gulp.watch('js/**/*.js', ['js']);
+  gulp.watch('scss/**/*.scss', ['scss']);
+  gulp.watch('images/src/**/*', ['image']);
 });
